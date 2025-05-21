1bc5e36e33187df9bbd752f297d19b8ac2c7c0c9
Anthony Villano <anthony.villano@ucdenver.edu>
2025-05-21 11:59:51 -0600

    [ANV] added a test run of SOURCES with PuBe source

--- SOURCES/sources_running/data/raw_sources_IO/stanzas2columns_ngroups.sh
+++ SOURCES/sources_running/data/raw_sources_IO/stanzas2columns_ngroups.sh
@@ -2,6 +2,6 @@
 
 file=$1
 
-cat $file |grep -E 'Total Neutron Spectrum|Grand|Total S.F.|Multigroup' -A6 |awk 'BEGIN{a=1}{if($1+0==$1){for(i=1; i<NF; i++){printf("%1.4e  ",$i)}}else{printf("\n")}}' > test.txt
+cat $file |grep -E 'Total Neutron Spectrum|Grand|Total S.F.|Multigroup' -A7 |awk 'BEGIN{a=1}{if($1+0==$1){for(i=1; i<NF; i++){printf("%1.4e  ",$i)}}else{printf("\n")}}' > test.txt
 
 awk '{ for (i=1; i<=NF; i++) RtoC[i]= (i in RtoC?RtoC[i] OFS :"") $i; } END{ for (i=1; i<=NF; i++) print RtoC[i] }' test.txt
