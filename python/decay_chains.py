6fbb089dab4c6877d7b808de27578b883e8b51d5
cnhellman <cecilia.hellman@ucdenver.edu>
2025-06-26 15:25:52 -0600

    Update decay_chains.py

--- python/decay_chains.py
+++ python/decay_chains.py
@@ -27,3 +27,47 @@ class Isotope:
 
     def time_until_decay(self):
         return random.expovariate(math.log(2) / self.half_life)
+    
+    decay_data = {
+    "Ra_228": {
+        "half_life": 5.7 * 365 * 24 * 3600,
+        "decay_modes": [("beta", "Ac_228", 1.0)]
+    },
+    "Ac_228": {
+        "half_life": 6.1 * 60,
+        "decay_modes": [("beta", "Th_228", 1.0)]
+    },
+    "Th_228": {
+        "half_life": 1.9 * 365 * 24 * 3600,
+        "decay_modes": [("alpha", "Ra_224", 1.0)]
+    },
+    "Ra_224": {
+        "half_life": 3.6 * 24 * 3600,
+        "decay_modes": [("alpha", "Rn_220", 1.0)]
+    },
+    "Rn_220": {
+        "half_life": 55,
+        "decay_modes": [("alpha", "Po_216", 1.0)]
+    },
+    "Po_216":{
+        "half_life":  0.14,
+        "decay_modes": [("alpha", "Pb_212", 1.0)]
+    },
+    "Pb_212":{
+        "half_life":  10.6 * 3600,
+        "decay_modes": [("beta", "Bi_212", 1.0)]
+    },
+    "Bi_212":{
+        "half_life":  61 * 60,
+        "decay_modes": [("beta", "Po_212", 0.6406),
+                        ("alpha", "Tl_208", 0.3594)]
+    },
+    "Po_212":{
+        "half_life":  3e-7,
+        "decay_modes": [("alpha","Pb_208", 1.0)]
+    },
+    "Tl_208":{
+        "half_life": 3.1 * 60,
+        "decay_modes": [("beta", "Pb_208", 1.0)]}
+    # Pb 208 is stable define separately
+}
