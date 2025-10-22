1b1a5c6df57e39b4a016cc3fc8ecb7ffe6b78880
buggyhellman <cecilia.hellman@ucdenver.edu>
2025-10-22 12:34:37 -0600

    shielding funcs

--- python/shielding.py
+++ python/shielding.py
@@ -1,6 +1,7 @@
 #library to compute shielding functions by J.O. Wallace (1976) 
 #see N-MISC-25-001
 import numpy as np
+import ENDF6el as endfel
 import scipy.integrate as integrate
 
 def sec(theta):
@@ -17,8 +18,8 @@ def slabFluxI(a,l1,l2,l3,mus=1.0):
 
   integrand = lambda theta,phi: np.cos(theta)*(1-np.exp(-mus*l3*sec(phi)*sec(theta)))
 
-  phiint = lambda phi: integrate.quad(integrand,0,np.math.atan(l2/((a+l3)*sec(phi))),args=(phi,))[0]
-  thetint = integrate.quad(phiint,0,np.math.atan(l1/(a+l3)))[0]
+  phiint = lambda phi: integrate.quad(integrand,0,np.atan(l2/((a+l3)*sec(phi))),args=(phi,))[0]
+  thetint = integrate.quad(phiint,0,np.atan(l1/(a+l3)))[0]
 
   return (1/(4*np.pi*mus))*thetint 
 
@@ -27,8 +28,8 @@ def slabFluxII(a,l1,l2,l3,mus=1.0):
 
   integrand = lambda theta,phi: np.cos(theta)*(1-np.exp(-mus*(l1*cot(phi)-a)*sec(phi)*sec(theta)))
 
-  phiint = lambda phi: integrate.quad(integrand,0,np.math.atan(l2/((a+l3)*sec(phi))),args=(phi,))[0]
-  thetint = integrate.quad(phiint,np.math.atan(l1/(a+l3)),np.math.atan(l1/a))[0]
+  phiint = lambda phi: integrate.quad(integrand,0,np.atan(l2/((a+l3)*sec(phi))),args=(phi,))[0]
+  thetint = integrate.quad(phiint,np.atan(l1/(a+l3)),np.atan(l1/a))[0]
 
   return (1/(4*np.pi*mus))*thetint 
 
@@ -37,8 +38,8 @@ def slabFluxIII(a,l1,l2,l3,mus=1.0):
 
   integrand = lambda theta,phi: np.cos(theta)*(1-np.exp(-mus*(l2*cot(theta)-a)*sec(phi)*sec(theta)))
 
-  phiint = lambda phi: integrate.quad(integrand,np.math.atan(l2/((a+l3)*sec(phi))),np.math.atan(l2/(a*sec(phi))),args=(phi,))[0]
-  thetint = integrate.quad(phiint,0,np.math.atan(l1/(a+l3)))[0]
+  phiint = lambda phi: integrate.quad(integrand,np.atan(l2/((a+l3)*sec(phi))),np.atan(l2/(a*sec(phi))),args=(phi,))[0]
+  thetint = integrate.quad(phiint,0,np.atan(l1/(a+l3)))[0]
 
   return (1/(4*np.pi*mus))*thetint 
 
@@ -47,10 +48,20 @@ def slabFluxIV(a,l1,l2,l3,mus=1.0):
 
   integrand = lambda theta,phi: np.cos(theta)*(1-np.exp(-mus*(l1*cot(phi)-a)*sec(phi)*sec(theta)))
 
-  phiint = lambda phi: integrate.quad(integrand,np.math.atan(l2/((a+l3)*sec(phi))),np.math.atan(l2/(l1*csc(phi))),args=(phi,))[0]
-  thetint = integrate.quad(phiint,np.math.atan(l1/(a+l3)),np.math.atan(l1/a))[0]
+  phiint = lambda phi: integrate.quad(integrand,np.atan(l2/((a+l3)*sec(phi))),np.atan(l2/(l1*csc(phi))),args=(phi,))[0]
+  thetint = integrate.quad(phiint,np.atan(l1/(a+l3)),np.atan(l1/a))[0]
 
   return (1/(4*np.pi*mus))*thetint 
 
 def slabFlux(a,l1,l2,l3,mus=1.0):
   return (slabFluxI(a,l1,l2,l3,mus=mus)+slabFluxII(a,l1,l2,l3,mus=mus)+slabFluxIII(a,l1,l2,l3,mus=mus)+slabFluxIV(a,l1,l2,l3,mus=mus))
+
+def getEs(): 
+
+    
+def Edep(a, l1, l2, l3, energies)
+    #for each energy
+    #get total xn
+    #run previous function (slab flux)
+    #populate flux vector
+    #return fluxvector
