68201281afc03ddfc621d545c3e036c51318d876
Anthony Villano <anthony.villano@ucdenver.edu>
2026-01-15 15:33:11 -0700

    [ANV] added all HDPE outputs

---
+++ 2-Flux/SNOLAB_Flux/SOURCES_outputs/stanzas2columns.sh
@@ -0,0 +1,7 @@
+#! /bin/sh
+
+file=$1
+
+cat $file |grep -E 'Total Neutron Spectrum|Grand|S.F.|Total S.F.|Multigroup' -A13 |awk 'BEGIN{a=1}{if($1+0==$1){for(i=1; i<NF; i++){printf("%1.4e  ",$i)}}else{printf("\n")}}' > test.txt
+
+awk '{ for (i=1; i<=NF; i++) RtoC[i]= (i in RtoC?RtoC[i] OFS :"") $i; } END{ for (i=1; i<=NF; i++) print RtoC[i] }' test.txt
